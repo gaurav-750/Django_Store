cd069d3994f4de1ad03ef1cd0342dd194aed2519
Gaurav <gauravsomani52750@gmail.com>
2023-01-30 11:27:40 +0530

    many-many, generic relationship

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 
 # Create your models here.
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    # todo To define a generic rel. we need 3 fields:
+    # Type -> Product, article, Collection, Video..etc
+    # ID
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
