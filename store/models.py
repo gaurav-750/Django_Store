a830e57c9c220ac3e86a7bb4234a124ad5241bc5
Gaurav <gauravsomani52750@gmail.com>
2023-01-31 10:07:41 +0530

    created and run migrations

--- store/models.py
+++ store/models.py
@@ -15,8 +15,9 @@ class Promotion(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)  # varchar(255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
 
@@ -46,6 +47,12 @@ class Customer(models.Model):
     # choice field
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+    class Meta:
+        db_table = 'store_customers'
+        indexes = [
+            models.Index(fields=['last_name', 'first_name'])
+        ]
 
 
 class Address(models.Model):
