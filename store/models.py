fa8d3a833c60e725704e48a42e6f491de0cbaf3e
Gaurav <gauravsomani52750@gmail.com>
2023-01-31 10:13:20 +0530

    revert migrations

--- store/models.py
+++ store/models.py
@@ -48,12 +48,6 @@ class Customer(models.Model):
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
-    class Meta:
-        db_table = 'store_customers'
-        indexes = [
-            models.Index(fields=['last_name', 'first_name'])
-        ]
-
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
