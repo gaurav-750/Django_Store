9f7cad5b53cd53b8ed2ac0aa90219b0292493bc0
Gaurav <gauravsomani52750@gmail.com>
2023-02-01 18:21:43 +0530

    django orm: Q, F expressions

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q, F
 
 from store.models import Product
 
@@ -14,7 +15,17 @@ def say_hello(req):
 
     # queryset = Product.objects.filter(title__contains='coffee')
     # queryset = Product.objects.filter(title__icontains='coffee')
-    queryset = Product.objects.filter(last_update__year=2021)
-    print('QSET:', queryset)
+    # queryset = Product.objects.filter(last_update__year=2021)
+
+    # *Products: inventory < 10 AND price < 20
+    # queryset = Product.objects.filter(
+    # inventory__lt=10).filter(unit_price__lt=20)
+
+    # *Products: inventory < 10 OR price < 20
+    # queryset = Product.objects.filter(
+    #     Q(inventory__lt=10) | Q(unit_price__lt=20))
+
+    # *Products: inventory = unit_price
+    queryset = Product.objects.filter(inventory=F('unit_price'))
 
     return render(req, 'hello.html', {'name': 'Gaurav', 'result': list(queryset)})
