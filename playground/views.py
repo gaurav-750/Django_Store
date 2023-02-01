248b1f97408e32fc4372a382c863553cd51336dc
Gaurav <gauravsomani52750@gmail.com>
2023-02-01 19:38:38 +0530

    django orm: aggregation

--- playground/views.py
+++ playground/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 
 from store.models import Product, Customer, Collection, Order, OrderItem
 
@@ -50,4 +51,27 @@ def say_hello(req):
     # *Order items for products in collection 3
     # queryset = OrderItem.objects.filter(product__collection_id=3)
 
-    return render(req, 'hello.html', {'name': 'Gaurav', 'result': list(queryset)})
+    # queryset = Product.objects.defer('description')
+
+    # *Selecting Related Tables:
+    # queryset = Product.objects.select_related('collection').all()
+
+    # *Aggregation:
+    # result = Product.objects.aggregate(
+    #     count=Count('id'), min_price=Min('unit_price'))
+
+    # *How many orders do we have?
+    # result = Order.objects.aggregate(total_orders=Count('id'))
+
+    # *How many units of product 1 have we sold?
+    # result = OrderItem.objects.filter(product_id=1).aggregate(
+    #     total_units_sold=Sum('quantity'))
+
+    # *How many orders has customer 1 placed?
+    # result = Order.objects.filter(customer_id=1).aggregate(Count('id'))
+
+    # *What is the min, max and average price of the products in collection 3?
+    # result = Product.objects.filter(
+    #     collection_id=3).aggregate(min=Min('unit_price'), max=Max('unit_price'), avg=Avg('unit_price'))
+
+    return render(req, 'hello.html', {'name': 'Gaurav', 'result': result})
