36b19dcdf971c721e90b4c089791216a651dbc3d
Gaurav <gauravsomani52750@gmail.com>
2023-02-01 16:46:33 +0530

    connecting django to mysql

--- store_project/settings.py
+++ store_project/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'store_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'pass123'
     }
 }
 
