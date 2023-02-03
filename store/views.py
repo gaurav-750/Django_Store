ff5d6ecafabb60497d14b924574f94cda5f527d4
Gaurav <gauravsomani52750@gmail.com>
2023-02-03 17:55:39 +0530

    serializing objects and returning them

--- store/views.py
+++ store/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+# from rest_framework.status import HTTP_404_NOT_FOUND
+
+from .models import Product
+from .serializers import ProductSerializer
 
 
 # Create your views here.
 @api_view()
 def product_list(req):
-    return Response({'message': 'Hello'})
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view()
 def product_detail(req, pk):
-    return Response({'message': 'prod detail', 'id': pk})
+    product = get_object_or_404(Product, pk=pk)
+    # it'll convert our product obj into dictionary
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
