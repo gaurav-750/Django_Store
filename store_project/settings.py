dd4854402966f44c9e0719ca84ed9514e009dd47
Gaurav <gauravsomani52750@gmail.com>
2023-02-03 16:58:49 +0530

    created new database

--- store_project/settings.py
+++ store_project/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'store_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront',
+        'NAME': 'storefront2',
         'HOST': 'localhost',
         'USER': 'root',
         'PASSWORD': 'pass123'
