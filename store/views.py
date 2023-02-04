ebe5bb3b766c593fd27993b39dc15d0d6ba73e82
Gaurav <gauravsomani52750@gmail.com>
2023-02-04 11:13:27 +0530

    created ProductViewset, CollectionViewset inheriting (Model)Viewset

--- store/views.py
+++ store/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 
 from .models import Product, Collection
@@ -13,25 +14,15 @@ from .serializers import ProductSerializer, CollectionSerializer
 
 
 # Create your views here.
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.select_related('collection').all()
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-    # def get_queryset(self):
-    # return Product.objects.select_related('collection').all()
-
-    # def get_serializer_class(self):
-    #     return ProductSerializer
 
     def get_serializer_context(self):
         return {
             'request': self.request,
         }
-
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
 
     def delete(self, req, pk):
         product = get_object_or_404(Product, pk=pk)
@@ -42,17 +33,10 @@ class ProductDetail(RetrieveUpdateDestroyAPIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class CollectionList(ListCreateAPIView):
+class CollectionViewset(ModelViewSet):
     queryset = Collection.objects.annotate(
         products_count=Count('products')
     )
-
-    serializer_class = CollectionSerializer
-
-
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
-    # GET, PUT
-    queryset = Collection.objects.annotate(products_count=Count('products'))
     serializer_class = CollectionSerializer
 
     def delete(self, req, pk):
@@ -63,3 +47,55 @@ class CollectionDetail(RetrieveUpdateDestroyAPIView):
 
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# class ProductList(ListCreateAPIView):
+#     queryset = Product.all()
+#     serializer_class = ProductSerializer
+
+#     # def get_queryset(self):
+#     # return Product.objects.select_related('collection').all()
+
+#     # def get_serializer_class(self):
+#     #     return ProductSerializer
+
+#     def get_serializer_context(self):
+#         return {
+#             'request': self.request,
+#         }
+
+
+# class ProductDetail(RetrieveUpdateDestroyAPIView):
+#     # queryset = Product.objects.all()
+#     # serializer_class = ProductSerializer
+
+#     def delete(self, req, pk):
+#         product = get_object_or_404(Product, pk=pk)
+#         if product.orderitem_set.count() > 0:
+#             return Response({"error": "Order items are related to this product"},
+#                             status=status.HTTP_405_METHOD_NOT_ALLOWED)
+#         product.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# class CollectionList(ListCreateAPIView):
+#     queryset = Collection.objects.annotate(
+#         products_count=Count('products')
+#     )
+
+#     serializer_class = CollectionSerializer
+
+
+# class CollectionDetail(RetrieveUpdateDestroyAPIView):
+#     # GET, PUT
+#     queryset = Collection.objects.annotate(products_count=Count('products'))
+#     serializer_class = CollectionSerializer
+
+#     def delete(self, req, pk):
+#         collection = get_object_or_404(Collection, pk=pk)
+#         if collection.products.count() > 0:
+#             return Response({"error": "Products are related to this collection!"},
+#                             status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+#         collection.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
