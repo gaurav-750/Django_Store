18e7b29168f7ff16a0a3c062b9bf4bdabc466203
Gaurav <gauravsomani52750@gmail.com>
2023-02-04 11:01:10 +0530

    using generic views in productdetail, collectiondetail

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import ProductList, ProductDetail, CollectionList, collection_detail
+from .views import ProductList, ProductDetail, CollectionList, CollectionDetail
 
 urlpatterns = [
     path('products/', ProductList.as_view()),
     path('products/<int:pk>', ProductDetail.as_view()),
     path('collections/', CollectionList.as_view()),
-    path('collections/<int:pk>/', collection_detail, name='collection-detail'),
+    path('collections/<int:pk>/', CollectionDetail.as_view(),
+         name='collection-detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework import status
 
 from .models import Product, Collection
@@ -29,19 +29,9 @@ class ProductList(ListCreateAPIView):
         }
 
 
-class ProductDetail(APIView):
-    def get(self, req, pk):
-        product = get_object_or_404(Product, pk=pk)
-        # it'll convert our product obj into dictionary
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-
-    def put(self, req, pk):
-        product = get_object_or_404(Product, pk=pk)
-        serializer = ProductSerializer(product, data=req.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class ProductDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
     def delete(self, req, pk):
         product = get_object_or_404(Product, pk=pk)
@@ -59,54 +49,17 @@ class CollectionList(ListCreateAPIView):
 
     serializer_class = CollectionSerializer
 
-# @api_view(['GET', 'POST'])
-# def collection_list(req):
-#     if req.method == 'GET':
-#         collections = Collection.objects.annotate(
-#             products_count=Count('products'))
-#         serializer = CollectionSerializer(collections, many=True)
 
-#         return Response(serializer.data)
-#     elif req.method == 'POST':
-#         # deserialization
-#         serializer = CollectionSerializer(data=req.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
+class CollectionDetail(RetrieveUpdateDestroyAPIView):
+    # GET, PUT
+    queryset = Collection.objects.annotate(products_count=Count('products'))
+    serializer_class = CollectionSerializer
 
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def collection_detail(req, pk):
-    collection = get_object_or_404(Collection.objects.annotate(
-        products_count=Count('products')), pk=pk)
-    if req.method == 'GET':
-        serializer = CollectionSerializer(collection)
-        return Response(serializer.data)
-    elif req.method == 'PUT':
-        serializer = CollectionSerializer(collection, data=req.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(status=status.HTTP_201_CREATED)
-    elif req.method == 'DELETE':
+    def delete(self, req, pk):
+        collection = get_object_or_404(Collection, pk=pk)
         if collection.products.count() > 0:
             return Response({"error": "Products are related to this collection!"},
                             status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-# @api_view(['GET', 'POST'])
-# def product_list(req):
-#     if req.method == 'GET':
-
-#     elif req.method == 'POST':
-
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def product_detail(req, pk):
-#     if req.method == 'GET':
-
-#     elif req.method == 'PUT':
-
-#     elif req.method == 'DELETE':
