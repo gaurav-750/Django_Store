3b521506a83b098a98e4d5d385c663cd8307de8e
Gaurav <gauravsomani52750@gmail.com>
2023-02-06 10:26:44 +0530

    added djoser, drf_simplejwt(3rd party lib) for authentication

--- store_project/settings.py
+++ store_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # 3rd Party
     'rest_framework',
+    'djoser',
     'debug_toolbar',
     'django_filters',
 
@@ -146,7 +147,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',),
+}
 
 AUTH_USER_MODEL = 'core.User'
