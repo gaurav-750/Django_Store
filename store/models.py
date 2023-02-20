82653b5c6bc9ab8218d2d36b40b99df2d4358646
Gaurav <gauravsomani52750@gmail.com>
2023-02-20 16:00:17 +0530

    created a ProductImage model, installed pillow

--- store/models.py
+++ store/models.py
@@ -45,6 +45,12 @@ class Product(models.Model):
         return self.title
 
 
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='store/images')
+    # image will be stored in /media/store/images/
+
+
 class Review(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name='reviews')
