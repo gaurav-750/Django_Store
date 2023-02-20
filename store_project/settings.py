7ed3a9bfddf7bc3af3398ff1d665f1d8f351f4e4
Gaurav <gauravsomani52750@gmail.com>
2023-02-20 15:47:51 +0530

    adding the endpt for images, also telling django where images will be stored

--- store_project/settings.py
+++ store_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,6 +140,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# *this is the endpt, at which we want to expose our user uploaded files:
+MEDIA_URL = '/media/'
+
+# *we shld also tell django where this media files are stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
