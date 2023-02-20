daab01cfd0f83953d1d4541470aa30e9a8958a5d
Gaurav <gauravsomani52750@gmail.com>
2023-02-20 19:01:20 +0530

    added corsheaders to django, so that other domains can access the api

--- store_project/settings.py
+++ store_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # 3rd Party
+    'corsheaders',
     'rest_framework',
     'djoser',
     'debug_toolbar',
@@ -54,6 +55,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -68,6 +70,11 @@ INTERNAL_IPS = [
     # ...
     "127.0.0.1",
     # ...
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8001',
+    'http://127.0.0.1:8001'
 ]
 
 ROOT_URLCONF = 'store_project.urls'
