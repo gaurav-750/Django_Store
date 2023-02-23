fcd2d875e33a452977b260c9c7074224a19d0b25
Gaurav <gauravsomani52750@gmail.com>
2023-02-23 09:09:49 +0530

    second test: non admin user creating collection

--- store/tests/test_collection.py
+++ store/tests/test_collection.py
@@ -7,8 +7,16 @@ import pytest
 @pytest.mark.django_db
 class TestCreateCollection:
 
-    def test_if_user_is_anonymous(self):
+    def test_if_user_is_anonymous_returns_401(self):
         client = APIClient()
         res = client.post('/store/collections/', {'title': 'a'})
 
         assert res.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_non_admin_returns_403(self):
+        client = APIClient()
+        # to authenticate the user
+        client.force_authenticate(user={})
+        res = client.post('/store/collections/', {'title': 'a'})
+
+        assert res.status_code == status.HTTP_403_FORBIDDEN
