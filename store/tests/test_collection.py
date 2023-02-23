8b57ad138cac3762222e2d279b165c0c01039433
Gaurav <gauravsomani52750@gmail.com>
2023-02-23 09:21:21 +0530

    remaining tests in collection endpt: admin+invalid data, admin+valid data

--- store/tests/test_collection.py
+++ store/tests/test_collection.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+
 from rest_framework import status
 from rest_framework.test import APIClient
 
@@ -20,3 +22,19 @@ class TestCreateCollection:
         res = client.post('/store/collections/', {'title': 'a'})
 
         assert res.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_invalid_returns_400(self):
+        client = APIClient()
+        # *to authenticate the user as ADMIN
+        client.force_authenticate(user=User(is_staff=True))
+        res = client.post('/store/collections/', {'title': ''})
+
+        assert res.status_code == status.HTTP_400_BAD_REQUEST
+        assert res.data['title'] is not None
+
+    def test_if_data_is_valid_returns_201(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        res = client.post('/store/collections/', {'title': 'a'})
+
+        assert res.status_code == status.HTTP_201_CREATED
