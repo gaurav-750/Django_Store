872948231b3b3ca7bd0c6774b464f34e2dabf935
Gaurav <gauravsomani52750@gmail.com>
2023-02-25 09:28:39 +0530

    optimization

--- store/views.py
+++ store/views.py
@@ -110,7 +110,8 @@ class CartItemViewset(ModelViewSet):
     http_method_names = ['get', 'post', 'patch', 'delete']
 
     def get_queryset(self):
-        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])
+        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk']) \
+            .select_related('product')
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
