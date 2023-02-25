749eb2f78d1809e065c8f6eb623c6052762a89ab
Gaurav <gauravsomani52750@gmail.com>
2023-02-25 19:13:55 +0530

    collecting static files for production

--- store_project/settings.py
+++ store_project/settings.py
@@ -159,6 +159,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # *this is the endpt, at which we want to expose our user uploaded files:
 MEDIA_URL = '/media/'
